f405680a479a64242b9fb158997823c004df495b
spacesnr <spacesnr@gmail.com>
2022-11-20 22:44:24 +0100

    created newfile.py

---
+++ newfile.py
@@ -0,0 +1,22 @@
+
+def add(x=0,y=0):
+    a=6
+    print(x+y)
+    print()
+    print(y-x+a)
+
+
+'''if __name__=="__main__": 
+    add()'''
+
+
+def sub(x=0,y=0):
+    print(y-x)
+    print()
+    print(x+y)
+
+num = int(input("\nInsert a number: "))
+
+sub(4,6)
+add(10,num)
+
