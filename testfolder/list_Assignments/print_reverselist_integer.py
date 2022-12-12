7119fd0c74e7b96a165adee32e2e4db974ac5f02
spacesnr <spacesnr@gmail.com>
2022-12-12 11:43:24 +0100

    printing integers in reverse

--- testfolder/list_Assignments/print_reverselist_integer.py
+++ testfolder/list_Assignments/print_reverselist_integer.py
@@ -16,23 +16,3 @@ list_fx(a_list)
 
 
 
-
-"""
-a_list = [1,2,3,4,5,7.3,"b"]
-
-
-def list_fx(a_list):
-    a_list=a_list.reverse()
-    if(type(a_list)==list):
-        
-        for char in a_list:
-            if(type(char)== int):
-                print(char, end=" ")
-            else:
-                continue
-    else:
-        print("no list")
-    
-list_fx(a_list)
-
-"""
