433f9cb5cc7b1554692f477672ce2d29d1483912
spacesnr <spacesnr@gmail.com>
2022-12-13 22:52:19 +0100

    cleaner code for printing integer

--- testfolder/list_Assignments/print_listInteger.py
+++ testfolder/list_Assignments/print_listInteger.py
@@ -3,13 +3,35 @@
 a_list = [1,2,"4",3,4,5,3.8]
 
 def list_fx(a):
+    list = []
     for char in a:
         if(type(char) == int):
-            print(char, end=" ")
-        elif(type(char)== str):
-            print("{}s".format(char), end=" ")
+            list.append(char)
         else:
-            print("\ni dont recognise this character")
-list_fx(a_list)
+            continue
+    return list
+       
+print(list_fx(a_list))
 
+
+
+#Re-writing the same above code, but with 2 functions. One function taking a list & a function as arguments.
+#function to check for integers
+def check_integer(num):
+    if type(num) == int:
+        return True
+    else:
+        return False
+
+#function to add integers to the list
+def list_func(list, func):
+    int_list = []
+
+    for i in list:
+        if func(i):
+            int_list.append(i)
+    return int_list
+
+list = [1,2,"4",3,4,5,3.8]
+print(list_func(list, check_integer))    
 
