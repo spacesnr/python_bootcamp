8526fe871b14ee517f68460e728943556ae82810
spacesnr <spacesnr@gmail.com>
2022-12-20 16:39:48 +0100

    Using reduce function for factorial

---
+++ testfolder/map_function2.py
@@ -0,0 +1,11 @@
+from functools import reduce
+#Program using reduce for factorial
+
+def fac(n,y=1):
+    
+    if  (n==0 or n==1):
+        return y
+    else:
+        return n*fac(n-1)
+#print(fac(5,1))
+print(reduce(fac,[5,0]))
