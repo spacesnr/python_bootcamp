b0ae4258e3bbeeed9f2a61abaad4cb31bc32ea43
spacesnr <spacesnr@gmail.com>
2023-01-03 23:15:51 +0100

    Coin flipping code

--- testfolder/list_Assignments/coin_flip.py
+++ testfolder/list_Assignments/coin_flip.py
@@ -5,8 +5,9 @@ import random
 coin_list = []
 
 for i in range(1,101):
-    coin_list += random.choice(["H","T"])
-print(coin_list)
+    coin_list += random.choice(["H","T","z"])
+
 
 print("Heads :", coin_list.count("H"))
 print("Tails :", coin_list.count("T"))
+print("Tails :", coin_list.count("z"))
