75e9667020c4fa97570de0164aa81b3a93810a72
spacesnr <spacesnr@gmail.com>
2023-03-18 23:10:58 +0100

    guessing game

--- testfolder/Assignment2/number_guesser_comp.py
+++ testfolder/Assignment2/number_guesser_comp.py
@@ -1,6 +1,6 @@
 #Select a number from 1-50 and let the computer guess your number
 import random
-comp_choice = random.randint(1,51)
+comp_choice = random.randint(1,50) #randint() is different from randrang() because randint() will include the last value (50) but rndrang() will stop at the value before the last digit.
 user_choice = int(input("Please a number (1-50) : "))
 
 if comp_choice == user_choice:
