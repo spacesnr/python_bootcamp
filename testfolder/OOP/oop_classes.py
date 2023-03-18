dfaf43768cf3af4f4cfe4492af1c0fcea64bc9cd
spacesnr <spacesnr@gmail.com>
2023-03-18 23:14:51 +0100

    OOP classes

--- testfolder/OOP/oop_classes.py
+++ testfolder/OOP/oop_classes.py
@@ -3,5 +3,8 @@ from phone_class import phone
 
 #item.instantiate_class()
 item1 = item("cup", 5000, 8)
-item1.name = "iron nig"
-print(item1.name)
+item2 = item("laptop",700, 10)
+
+print(item.calulate_price(item2))
+
+print(item2.name)
