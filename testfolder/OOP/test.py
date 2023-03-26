24c9b9c9fe23e4f045a443d51f10ecad9b4f0bf2
spacesnr <spacesnr@gmail.com>
2023-03-26 07:34:53 +0100

    test

--- testfolder/OOP/test.py
+++ testfolder/OOP/test.py
@@ -1,7 +1,20 @@
-def emeka(x):
+"""def emeka(x):
     emeka.counter = getattr(emeka, "counter", 5) +1
     #return 4
 
 for x in range(10):
     emeka(x)
-print(emeka.counter)
+print(emeka.counter) """
+Alist = ["kniff", "gun", "cat", "rice"]
+repr(Alist)
+def agu():
+    x = 5
+    print(x)
+    x = 6
+    print(x)
+#agu()    
+#def next():
+  #  print(x)
+
+#next()
+
