385238116941b32e452d91d1d9d175bacd3d184f
spacesnr <spacesnr@gmail.com>
2023-03-26 07:33:30 +0100

    a fun hangman game

--- testfolder/Assignment2/hangman_game.py
+++ testfolder/Assignment2/hangman_game.py
@@ -1,49 +1,77 @@
 import random
+#from words import word_list
 
-print("Welcome to the Hangman Game. Guess a fruit")
+def HangmanGame():
 
-words = random.choice(["apple"])
-spaces = len(words)
-dashes = []
+    words = random.choice(["apple"])
+    spaces = len(words)
+    tries = 6
+    dashes = "_" * spaces
+    guessedLetters = []
+    guessedCorrect = 0
 
-def printDashes(i):
-    i = 0
-    while i < spaces:
-        dashes.append("_")
-        i +=1
+    print("\nWelcome to the Hangman Game. Guess a fruit")
 
+    while guessedCorrect == 0 and tries > 0:
+        user_input = str(input("\nEnter a word or letter : ")) # taking user input
 
-def list_duplicates_of(seq,item):
-    start_at = -1
-    locs = []
-    
-    while True:
-        
-        try:
-            loc = seq.index(item,start_at+1)
-        except ValueError:
-            break
+        if len(user_input) == 1 and user_input.isalpha(): 
+            if user_input in words:
+                guessedLetters.append(user_input)
+                listdashes = list(dashes)
+                index = [i for i, r in enumerate(words) if r == user_input] # to get the index of each user input
+                
+                for x in index:
+                    listdashes[x] = user_input #Placing the user input on the right dashes
+                    dashes = "".join(listdashes)
+                
+                if "_" not in dashes:
+                    #print("yes!!!")
+                    guessedCorrect = 1
+            #    print(f"You already guessed {user_input}")
+            else:
+                #prints when you guessed a wrong letter
+                print("You guessed a wrong letter")
+                tries -= 1
+                guessedLetters.append(user_input)
+        elif len(user_input) == len(words): #When a user types in an entire word
+            
+            if user_input == words:
+                #print(f"\nCongratulations, you guessed {user_input} correctly\n")
+                guessedLetters.append(user_input)
+                guessedCorrect = 1
+                dashes = words
+            else: 
+                print("\nYou guessed wrongly\n")
+                tries -= 1
+                guessedLetters.append(user_input)
         else:
-            locs.append(loc)
-            start_at=loc
-    return print(locs)
+            print("\nYou guessed the wrong word\n")
+            tries -= 1
+            guessedLetters.append(user_input)
 
-        
+        print()
+        print(dashes)  
+        print(f"You have {tries} tries remaining and u've gussed : " + ", ".join(guessedLetters))
 
-user_input = str(input("Write an alphabet : "))
-if user_input in words:
-    print("ok na")
-    rr = words.index(user_input)
-    jj = words[rr]
-    printDashes(spaces)
-    list_duplicates_of(words, user_input)
-    
-    #dashes[rr] = user_input
-    
-    #print(" ".join(dashes))
-    
-else:
-    print("hmmmm")
+    if guessedCorrect == 0:
+        print("\nOh no, You couldnt save Morty!\n")
+    else:
+        print("\nCongratulations, Morty lives because of you!\n")
+
+def main():
+    HangmanGame()
+    while input("Do you wish to play again?(Y/N) : \n").upper() == "Y":
+        HangmanGame()
+    print("Bye, see you soon")
+
+main()
+
+
+
+
+
+
 
 
 
