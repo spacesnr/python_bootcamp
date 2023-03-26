9c7d9eb9cd3b9ba0bbd70dbb0a01294ed713e547
spacesnr <spacesnr@gmail.com>
2023-03-26 07:34:15 +0100

    a little robot class

--- testfolder/OOP/Robots.py
+++ testfolder/OOP/Robots.py
@@ -1,9 +1,28 @@
 """A class to create Robots"""
 
+#def introduce():
+   # print(f"\nhi i'm {x.name} \n")
+
 class Robot:
-    def __init__(self, color="black",func="maths genuis"):
-        self.color = color
-        self.func = func
+   def __init__(self, color="black",func="maths genuis"):
+      self.color = color
+      self.func = func
+
+   def introduce(self):
+      if self.name:
+         print(f"hi, welcome {self.name}, you are a {self.gender} !")
+      else:
+         print("child!")
+      
+      #  self.obj = obj
+       # print(f"\nhi i'm {obj.name} \n")
+
+   def setGender(self,gender):
+      self.gender = gender
+      
+   def getGender(self):
+      return self.gender
+   
 
 x = Robot()
 y = Robot()
@@ -11,11 +30,26 @@ z = Robot("yellow", "cooking")
 Robot.brand = "Starline"
 x.name = "ElonX"
 y.name = "stargate"
-getattr(x, "country","USA")
+
+y.setGender("male")
+x.setGender(y.getGender())
+
+
 
 x.buildYear = 1993
 y.buildYear = 2000
 
-print(f"\n{x.country}\n")
+#def introduce():
+#    print(f"\nhi i'm {y.name} \n")
 
-#getattr 
+print(f"\n{type(x).getGender(x)}\n")
+
+#Robot.introduce(y)
+y.introduce()
+x.introduce()
+#type(z).introduce(x)
+
+
+
+
+
